163d5baccbcf723510625d8d70730cc77c0f0bfe
Yasin <yasinarafat2413@gmail.com>
2025-06-11 14:49:39 +0600

    home page added

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
 import Rootlayout from "../layouts/Rootlayout";
+import Home from "../components/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
         Component: Rootlayout,
+        children:[
+            {
+                index: true,
+                Component: Home
+            },
+        ]
     },
 ]);
 
--- src/layouts/Rootlayout.jsx
+++ src/layouts/Rootlayout.jsx
@@ -5,7 +5,7 @@ import Footer from '../shared/Footer';
 
 const Rootlayout = () => {
     return (
-        <div>
+        <div className='max-w-11/12 mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
