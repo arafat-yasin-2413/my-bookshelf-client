a7dd7da32a49dc6fbad1ceb92c0d4eef1a30057e
Yasin <yasinarafat2413@gmail.com>
2025-06-14 12:37:29 +0600

    firebase config secured

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3NhPnd6mGpErDTvFKKoy_56tC-3f_CxE",
-  authDomain: "bookshelf-project-d43ac.firebaseapp.com",
-  projectId: "bookshelf-project-d43ac",
-  storageBucket: "bookshelf-project-d43ac.firebasestorage.app",
-  messagingSenderId: "716340608458",
-  appId: "1:716340608458:web:42a2191dbb42f4b4aea333"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
