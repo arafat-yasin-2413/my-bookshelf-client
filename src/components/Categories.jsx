0feb85dffc4eda9386d52953e13bcbd52f54f782
Yasin <yasinarafat2413@gmail.com>
2025-06-15 11:12:02 +0600

    simple category button designed

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,46 +1,42 @@
-import React, { use, useEffect, useState } from 'react';
-import { toast } from 'react-toastify';
-import { AuthContext } from '../contexts/AuthContext';
-import LoaderSpinner from './LoaderSpinner';
+import React, { use, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { AuthContext } from "../contexts/AuthContext";
+import LoaderSpinner from "./LoaderSpinner";
 
 const Categories = () => {
-
     const [categories, setCategories] = useState([]);
-    const {loading,setLoading} = use(AuthContext);
+    const { loading, setLoading } = use(AuthContext);
 
-    useEffect(()=>{
-        setLoading(true)
+    useEffect(() => {
+        setLoading(true);
         fetch(`${import.meta.env.VITE_API_URL}/categories`)
-        .then(res=>res.json())
-        .then(data => {
-            setCategories(data);
-            setLoading(false);
-            // console.log(data);
-            
-        })
-        .catch(error=>{
-            toast.error('Failed to fetch categories : ',error);
-            setLoading(false);
-        })
-    },[])
+            .then((res) => res.json())
+            .then((data) => {
+                setCategories(data);
+                setLoading(false);
+                // console.log(data);
+            })
+            .catch((error) => {
+                toast.error("Failed to fetch categories : ", error);
+                setLoading(false);
+            });
+    }, []);
 
     return (
-        <div>
-
-            <h2>Showing All categories</h2>
-
-            {
-                loading ? (<LoaderSpinner></LoaderSpinner>) : 
-                
-
-                (categories.length > 0) && (
-                    categories.map((category, idx)=>(
-                        <h5 key={idx}>{category}</h5>
+        <div className="my-10">
+            <h2>Showing All categories <span className="font-bold">({categories.length ? categories.length : 0}) </span></h2>
+            <div className="flex gap-3">
+                {loading ? (
+                    <LoaderSpinner></LoaderSpinner>
+                ) : (
+                    categories.length > 0 &&
+                    categories.map((category, idx) => (
+                        <button className="btn bg-blue-200 hover:bg-black hover:text-white text-xl px-16 py-10 rounded-2xl" key={idx}>
+                            {category}
+                        </button>
                     ))
-                )
-            }
-        
-            
+                )}
+            </div>
         </div>
     );
 };
