cba8d8de1bbfdb7259cf5b0604a86ef9fb2efe4c
Yasin <yasinarafat2413@gmail.com>
2025-06-17 11:47:11 +0600

    error page design done

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,23 +1,43 @@
 import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router";
+import errorImage from "/assets/404-error.png";
 
 const ErrorPage = () => {
-
     const location = useLocation();
-    useEffect(()=>{
+    useEffect(() => {
         document.title = "404";
-    },[location.pathname])
+    }, [location.pathname]);
 
     return (
-        <div className="w-3xl mx-auto border border-secondary my-10 p-4">
-            <h2>this is error page</h2>
+        <section class="bg-gray-200">
+            <div class="container flex items-center justify-center min-h-screen px-6 py-12 mx-auto">
+                <div class="w-full ">
+                    <div class="flex flex-col items-center max-w-lg mx-auto text-center">
 
-            <div className="mt-6">
-                <Link className="text-white font-bold p-2 bg-primary rounded" to="/">
-                    Go To Home Page
-                </Link>
+
+                        <div>
+                            <img src={errorImage} alt="" />
+                        </div>
+
+
+                        <h1 class="mt-3 text-2xl font-semibold md:text-3xl">
+                            Page not Found
+                        </h1>
+                        
+
+                        <div class="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+
+
+                            <Link to="/" class="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                                Take me home
+                            </Link>
+                        </div>
+                    </div>
+
+                    
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
