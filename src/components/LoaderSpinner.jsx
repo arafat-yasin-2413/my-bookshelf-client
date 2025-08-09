24e0e4fc1627bcd51ff3a2138a138bed236c6575
Yasin <yasinarafat2413@gmail.com>
2025-08-09 17:39:51 +0600

    loader spinner updated

--- src/components/LoaderSpinner.jsx
+++ src/components/LoaderSpinner.jsx
@@ -1,22 +1,12 @@
-import { motion } from "framer-motion";
 import React from "react";
+import { ClipLoader } from "react-spinners";
+
 
 const LoaderSpinner = () => {
     return (
-        <motion.div
-            className="h-screen flex items-center justify-center w-full"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.4 }}
-        >
-            <motion.span
-                className="loading loading-bars loading-xl"
-                initial={{ scale: 0.8, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{ duration: 0.3, ease: "easeOut" }}
-            ></motion.span>
-        </motion.div>
+        <div className="flex justify-center items-center min-h-screen bg-base-200">
+            <ClipLoader color="#36d7b7" size={50} />
+        </div>
     );
 };
 
