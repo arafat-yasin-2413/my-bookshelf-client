25576fb70f4ad5d421567fc4edceba60a8960ce9
Yasin <yasinarafat2413@gmail.com>
2025-08-17 14:20:49 +0600

    see details button added in bookCard

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BiUpvote } from "react-icons/bi";
+import { FaRegEye } from "react-icons/fa";
 import { Link } from "react-router";
 
 const BookCard = ({ book }) => {
@@ -11,17 +12,39 @@ const BookCard = ({ book }) => {
         <div className="shadow rounded-xl w-4/5 sm:w-full flex flex-col ">
             {/* image div */}
             <div className="bg-white rounded-t-xl w-full flex justify-center items-center">
-                <img className="w-32 py-4 px-4" src={coverPhoto} alt={`image of ${bookTitle}`} />
+                <img
+                    className="w-32 py-4 px-4"
+                    src={coverPhoto}
+                    alt={`image of ${bookTitle}`}
+                />
             </div>
             {/* content div */}
-            <div className="mt-2 p-2">
+            <div className="mt-2 px-2 py-4">
                 <p className="mb-2 flex items-center bg-secondary w-fit px-1 py-0.5 rounded">
                     <BiUpvote className="text-primary"></BiUpvote>
-                    <span className="text-[0.8rem] font-semibold">{upvotedBy.length}</span>
+                    <span className="text-[0.8rem] font-semibold">
+                        {upvotedBy.length}
+                    </span>
                 </p>
                 <h3 className="text-[1.1rem] font-medium mb-2">{bookTitle}</h3>
-                <h4 className="text-gray-500 text-[0.8rem] mb-2">{bookAuthor}</h4>
-                <h5 className="text-[0.9rem] text-primary bg-white w-fit px-3 py-1 rounded-full outline outline-secondary">{bookCategory}</h5>
+                <h4 className="text-gray-500 text-[0.8rem] mb-2">
+                    {bookAuthor}
+                </h4>
+                <h5 className="text-[0.9rem] text-primary bg-white w-fit px-3 py-1 rounded-full outline outline-secondary">
+                    {bookCategory}
+                </h5>
+
+                <div className="bg-white w-fit px-1 py-0.5 mt-1 rounded shadow text-primary hover:bg-primary hover:text-white">
+                    <Link
+                        to={`/bookDetails/${_id}`}
+                        className="flex items-center gap-1"
+                    >
+                        <span className="">See Details</span>
+                        <span>
+                            <FaRegEye className=""></FaRegEye>
+                        </span>
+                    </Link>
+                </div>
             </div>
         </div>
     );
